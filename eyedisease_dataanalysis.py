ade07be09cffcfe2dac8b28a0477846e3153792f
satish kathiriya <satishkathiriya99@gmail.com>
2017-03-30 23:49:02 +0530

    Update eyedisease_dataanalysis.py

--- eyedisease_dataanalysis.py
+++ eyedisease_dataanalysis.py
@@ -1,7 +1,6 @@
 # Load libraries
 import pandas
 from pandas.tools.plotting import scatter_matrix
-import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
